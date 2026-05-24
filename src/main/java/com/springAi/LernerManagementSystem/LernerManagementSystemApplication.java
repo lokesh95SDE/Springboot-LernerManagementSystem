c4cd9f9796cb0552f99f28730bc63bf36d8c8102
Lokesh <lokesh95info@gmail.com>
2026-05-24 16:53:53 +0530

    Initial commit

---
+++ src/main/java/com/springAi/LernerManagementSystem/LernerManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.springAi.LernerManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LernerManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LernerManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springAi/LernerManagementSystem/LernerManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springAi.LernerManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LernerManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
