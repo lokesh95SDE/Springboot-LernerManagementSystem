abe574995bc4a62b6d41faefd9060e34d5e9b4e8
Lokesh <lokesh95info@gmail.com>
2026-05-29 00:04:00 +0530

    LearnerEntity

---
+++ src/main/java/com/springAi/LernerManagementSystem/entity/Learner.java
@@ -0,0 +1,56 @@
+package com.springAi.LernerManagementSystem.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Learner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long learnerId;
+    private String learnerName;
+    private String learnerEmail;
+    private String learnerPhone;
+
+    public Learner(Long learnerId, String learnerName, String learnerEmail, String learnerPhone) {
+        this.learnerId = learnerId;
+        this.learnerName = learnerName;
+        this.learnerEmail = learnerEmail;
+        this.learnerPhone = learnerPhone;
+    }
+
+    public Long getLearnerId() {
+        return learnerId;
+    }
+
+    public void setLearnerId(Long learnerId) {
+        this.learnerId = learnerId;
+    }
+
+    public String getLearnerName() {
+        return learnerName;
+    }
+
+    public void setLearnerName(String learnerName) {
+        this.learnerName = learnerName;
+    }
+
+    public String getLearnerEmail() {
+        return learnerEmail;
+    }
+
+    public void setLearnerEmail(String learnerEmail) {
+        this.learnerEmail = learnerEmail;
+    }
+
+    public String getLearnerPhone() {
+        return learnerPhone;
+    }
+
+    public void setLearnerPhone(String learnerPhone) {
+        this.learnerPhone = learnerPhone;
+    }
+
+}
