34e73240196a2462a5a8123e8503be5118af36b2
Lokesh <lokesh95info@gmail.com>
2026-07-08 00:06:15 +0530

    Authentication and User verification

--- src/main/java/com/springAi/LernerManagementSystem/LernerManagementSystemApplication.java
+++ src/main/java/com/springAi/LernerManagementSystem/LernerManagementSystemApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+//@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @SpringBootApplication
 @EnableCaching
 public class LernerManagementSystemApplication {
